f909966dcc0cd4ac7ca517e85ad53130e5fb4ac5
alexeyban <alexeyban@dd708e65-a195-3138-6e7a-1f6716c2db63>
2010-11-16 16:37:27 +0000

    Заменен файл БД, используются поля varbinary. После сканирования распакованные данные кэшируются. Для раскодирования байтового массива в текст используется собственный кодировщик.

--- trunk/SmartDeviceProject2/SmartDeviceProject2/ViewProductForm.Designer.cs
+++ trunk/SmartDeviceProject2/SmartDeviceProject2/ViewProductForm.Designer.cs
@@ -31,113 +31,41 @@
         {
             this.mainMenu1 = new System.Windows.Forms.MainMenu();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.splitter1 = new System.Windows.Forms.Splitter();
-            this.closeFrmBtn = new Bananamama.BackOffice.TSD.Client.ImageButton();
-            this.printBtn = new Bananamama.BackOffice.TSD.Client.ImageButton();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.printBtn = new Bananamama.BackOffice.TSD.Client.ImageButton();
+            this.closeFrmBtn = new Bananamama.BackOffice.TSD.Client.ImageButton();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.searchBtn = new Bananamama.BackOffice.TSD.Client.ImageButton();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.White;
-            this.panel1.Controls.Add(this.textBox5);
-            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.searchBtn);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
             this.panel1.Controls.Add(this.label4);
-            this.panel1.Controls.Add(this.textBox3);
             this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.textBox2);
             this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.textBox1);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.printBtn);
             this.panel1.Controls.Add(this.closeFrmBtn);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(221, 258);
-            // 
-            // splitter1
-            // 
-            this.splitter1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.splitter1.Location = new System.Drawing.Point(0, 258);
-            this.splitter1.Name = "splitter1";
-            this.splitter1.Size = new System.Drawing.Size(221, 100);
-            // 
-            // closeFrmBtn
-            // 
-            this.closeFrmBtn.BackColor = System.Drawing.Color.PaleGreen;
-            this.closeFrmBtn.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.closeFrmBtn.Location = new System.Drawing.Point(0, 217);
-            this.closeFrmBtn.Name = "closeFrmBtn";
-            this.closeFrmBtn.Size = new System.Drawing.Size(221, 41);
-            this.closeFrmBtn.TabIndex = 7;
-            this.closeFrmBtn.Text = "Назад";
-            this.closeFrmBtn.Click += new System.EventHandler(this.closeFrmBtn_Click);
-            this.closeFrmBtn.KeyDown += new System.Windows.Forms.KeyEventHandler(this.closeFrmBtn_KeyDown);
-            // 
-            // printBtn
-            // 
-            this.printBtn.BackColor = System.Drawing.Color.PaleGreen;
-            this.printBtn.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.printBtn.Location = new System.Drawing.Point(0, 176);
-            this.printBtn.Name = "printBtn";
-            this.printBtn.Size = new System.Drawing.Size(221, 41);
-            this.printBtn.TabIndex = 6;
-            this.printBtn.Text = "Печать";
-            this.printBtn.KeyDown += new System.Windows.Forms.KeyEventHandler(this.printBtn_KeyDown);
-            // 
-            // label1
-            // 
-            this.label1.Location = new System.Drawing.Point(0, 10);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 20);
-            this.label1.Text = "Название:";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(63, 7);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(158, 23);
-            this.textBox1.TabIndex = 1;
-            // 
-            // label2
-            // 
-            this.label2.Location = new System.Drawing.Point(0, 59);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(82, 20);
-            this.label2.Text = "Новая цена";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Enabled = false;
-            this.textBox2.Location = new System.Drawing.Point(76, 59);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(145, 23);
-            this.textBox2.TabIndex = 3;
-            // 
-            // label3
-            // 
-            this.label3.Location = new System.Drawing.Point(0, 85);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(82, 20);
-            this.label3.Text = "Старая цена";
-            // 
-            // textBox3
-            // 
-            this.textBox3.Enabled = false;
-            this.textBox3.Location = new System.Drawing.Point(76, 84);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(145, 23);
-            this.textBox3.TabIndex = 4;
+            this.panel1.Size = new System.Drawing.Size(238, 258);
             // 
             // label4
             // 
@@ -146,21 +74,116 @@
             this.label4.Size = new System.Drawing.Size(59, 20);
             this.label4.Text = "Артикул";
             // 
-            // textBox4
+            // label3
             // 
-            this.textBox4.Enabled = false;
-            this.textBox4.Location = new System.Drawing.Point(52, 110);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(169, 23);
-            this.textBox4.TabIndex = 5;
+            this.label3.Location = new System.Drawing.Point(0, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 20);
+            this.label3.Text = "Старая цена";
             // 
-            // textBox5
+            // label2
             // 
-            this.textBox5.Enabled = false;
-            this.textBox5.Location = new System.Drawing.Point(0, 33);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(221, 23);
-            this.textBox5.TabIndex = 2;
+            this.label2.Location = new System.Drawing.Point(0, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 20);
+            this.label2.Text = "Новая цена";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 20);
+            this.label1.Text = "Название:";
+            // 
+            // printBtn
+            // 
+            this.printBtn.BackColor = System.Drawing.Color.PaleGreen;
+            this.printBtn.BackgroundImage = null;
+            this.printBtn.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.printBtn.Location = new System.Drawing.Point(0, 176);
+            this.printBtn.Name = "printBtn";
+            this.printBtn.Size = new System.Drawing.Size(238, 41);
+            this.printBtn.TabIndex = 6;
+            this.printBtn.Text = "Печать";
+            this.printBtn.KeyDown += new System.Windows.Forms.KeyEventHandler(this.printBtn_KeyDown);
+            // 
+            // closeFrmBtn
+            // 
+            this.closeFrmBtn.BackColor = System.Drawing.Color.PaleGreen;
+            this.closeFrmBtn.BackgroundImage = null;
+            this.closeFrmBtn.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.closeFrmBtn.Location = new System.Drawing.Point(0, 217);
+            this.closeFrmBtn.Name = "closeFrmBtn";
+            this.closeFrmBtn.Size = new System.Drawing.Size(238, 41);
+            this.closeFrmBtn.TabIndex = 7;
+            this.closeFrmBtn.Text = "Назад";
+            this.closeFrmBtn.Click += new System.EventHandler(this.closeFrmBtn_Click);
+            this.closeFrmBtn.KeyDown += new System.Windows.Forms.KeyEventHandler(this.closeFrmBtn_KeyDown);
+            // 
+            // splitter1
+            // 
+            this.splitter1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.splitter1.Location = new System.Drawing.Point(0, 258);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(238, 21);
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.PaleGreen;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular);
+            this.label5.Location = new System.Drawing.Point(77, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(141, 20);
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.PaleGreen;
+            this.label6.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular);
+            this.label6.Location = new System.Drawing.Point(3, 30);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(215, 20);
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.PaleGreen;
+            this.label7.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular);
+            this.label7.Location = new System.Drawing.Point(77, 59);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(141, 20);
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.PaleGreen;
+            this.label8.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular);
+            this.label8.Location = new System.Drawing.Point(77, 85);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(141, 20);
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.PaleGreen;
+            this.label9.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular);
+            this.label9.Location = new System.Drawing.Point(77, 110);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(141, 20);
+            this.label9.Text = "label9";
+            // 
+            // searchBtn
+            // 
+            this.searchBtn.BackColor = System.Drawing.Color.PaleGreen;
+            this.searchBtn.BackgroundImage = null;
+            this.searchBtn.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.searchBtn.Location = new System.Drawing.Point(0, 135);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(238, 41);
+            this.searchBtn.TabIndex = 19;
+            this.searchBtn.Text = "Поиск";
+            this.searchBtn.Click += new System.EventHandler(this.searchBtn_Click);
+            this.searchBtn.KeyDown += new System.Windows.Forms.KeyEventHandler(this.searchBtn_KeyDown);
             // 
             // ViewProductForm
             // 
@@ -189,14 +212,15 @@
         private System.Windows.Forms.Splitter splitter1;
         private Bananamama.BackOffice.TSD.Client.ImageButton printBtn;
         private Bananamama.BackOffice.TSD.Client.ImageButton closeFrmBtn;
-        private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private Bananamama.BackOffice.TSD.Client.ImageButton searchBtn;
     }
 }
